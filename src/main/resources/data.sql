e3f3f257c3cc1fadc74fde248b956c402439fe87
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2020-09-23 12:42:38 -0300

    NAC finalizada

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (name, email, pass) VALUES 
-('Gabriel Biancardi', 'rm83607@fiap.com.br', '123456');
+INSERT INTO users (name, email, pass, GIT_HUB_USER) VALUES 
+('Gabriel Biancardi', 'rm83607@fiap.com.br', '123456', 'gbbiancardi');
 INSERT INTO tasks (title, description, point, status) VALUES
 ('Tarefa 01','Tarefa de teste', 80, 50);
